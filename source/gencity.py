fbaa4e146f1f6050584640cfad1498ce47f852b7
kkoehler4070 <koehler.62@gmail.com>
2023-05-22 05:43:13 -0400

    Update gencity.py
    Added comments to improve readability.

--- source/gencity.py
+++ source/gencity.py
@@ -18,9 +18,9 @@ class City:
 
         self.cityMap = ndarray((width,height),dtype='O')
 
-        # Place a starting intersection
-        currX = self.width // 2 #random.randint(0,width-1)
-        currY = self.height //2 #random.randint(0,height-1)
+        # Place a starting intersection in the middle of the map
+        currX = self.width // 2 
+        currY = self.height // 2
         self.cityMap[currY][currX] = '#'
 
         # Generate roads in all directions
@@ -29,21 +29,19 @@ class City:
         self.newRoad((currX,currY),'l')
         self.newRoad((currX,currY),'r')
 
+        # Currently the final checkCheck is commented out, because it is resulting in unpredicatble results.
         #self.finalCheck()
 
 
     def newIntersection(self,currentPosition,direction):
+        """This function decides whether to create a new intersection, straight road, or stop."""
+
         prob = random.randint(0,10)
-        print("Trying intersection ({},{})".format(currentPosition[0],currentPosition[1]))
 
         currX = currentPosition[0]
-        #if currX > self.width or currX < 0:
-        #    return
-
         currY = currentPosition[1]
-        #if currY > self.height or currY < 0:
-        #    return
 
+        # 30% probability of a new intersection, 40% probabiloity of a straight road, 30% probability that the road just ends
         if prob < 3:
             # New intersection
             self.cityMap[currY][currX] = '#'
@@ -71,7 +69,7 @@ class City:
 
 
     def newRoad(self,currentPosition, direction):
-        """Direction can be 'u','d','l','r'."""
+        """Creates a new road in direction. Direction can be 'u','d','l','r'."""
         if direction == 'u':
             vector = (0,-1)
             directionChar = '|'
@@ -85,27 +83,33 @@ class City:
             vector = (1,0)
             directionChar = '-'
 
-        randRoad = 5 #random.randint(5,10)
+        # Originally, this was intended as a random road length. However, this created
+        # unpredictable results. Therefore, we changed to a constant.
+        randRoad = 5
         for index in range(1,randRoad):
             currX = index*vector[0] + currentPosition[0]
             currY = index*vector[1] + currentPosition[1]
 
+            # If we are outside the width of the map, simply return
             if currX >= self.width or currX <0 or currY >= self.height or currY <0:
                 return
 
             if self.cityMap[currY][currX] == None:
                 self.cityMap[currY][currX] = directionChar
         
+        # Move to the next spot
         currX = currentPosition[0]+vector[0]*(randRoad)
         currY = currentPosition[1]+vector[1]*(randRoad)
         if currX >= self.width or currX <0 or currY >= self.height or currY <0:
                 return
 
+        # Call newIntersection to determine whether we should create a new intersection or not.
         if self.cityMap[currY][currX] == None:
             self.newIntersection((currX,currY),direction)
         
     def finalCheck(self):
-        # run through map and see if we need to add any intersections
+        """This method walks through the map to determine whether we should create any intersections. For example if
+        we have to different directions of road right next to each other."""
         for y in range(1,self.height-1):
             for x in range(1,self.width-1):
                 # if there is a road to the left or right and a road to the up or down, add an intersection
