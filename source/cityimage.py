5d5c8db4f0934407518b87b4a8b0a17d2e082842
kkoehler4070 <koehler.62@gmail.com>
2023-05-22 06:07:47 -0400

    Update cityimage.py
    Many changes to make this a class instead of a script that runs directly. Also removed the direct references to the assets and placed those in a test file.

--- source/cityimage.py
+++ source/cityimage.py
@@ -2,36 +2,49 @@ from PIL import Image
 import gencity
 import os
 
-# Load the spritesheet
-intersection = Image.open(os.getcwd() + "/kenney_isometric-city/PNG/cityTiles_089.png")
-intersection = intersection.convert("RGBA")
-up = Image.open(os.getcwd() + "/kenney_isometric-city/PNG/cityTiles_081.png")
-up = up.convert("RGBA")
-left = Image.open(os.getcwd() + "/kenney_isometric-city/PNG/cityTiles_073.png")
-left = left.convert("RGBA")
 
-# create a new image
-city = Image.new("RGBA",(2048,2048))
+class CityImage:
+    def loadIntersection(self,intersectionPath):
+        self.intersection = Image.open(intersectionPath)
+        self.intersection = self.intersection.convert("RGBA")
+    
+    def loadUp(self,upPath):
+        self.up = Image.open(upPath)
+        self.up = self.up.convert("RGBA")
 
-mapSize = 20
-cityMap = gen2.City()
-cityMap.generateCity(mapSize,mapSize)
-startPosition = (1024,1024)
-tile_width = 132
-tile_width_half = 64
-tile_height = 101
-tile_height_half = 32
+    def loadLeft(self,leftPath):
+        self.left = Image.open(leftPath)
+        self.left = self.left.convert("RGBA")
 
-for y in range(0,mapSize):
-    for x in range(0,mapSize):
-        position = ((x*tile_width_half)-(y*tile_width_half)+startPosition[0],(y*tile_height_half)+(x*tile_height_half)+startPosition[1])
+    def createCityImage(self,imageSize,mapSize):
+        """Creates a new city image of size imageSize (in pixels) and with underlying mapSize (in tiles).
+        imageSize and mapSize are tuples."""
+        
+        # create a new image    
+        city = Image.new("RGBA",imageSize)
 
-        if cityMap.cityMap[y][x] == '#':
-            city.alpha_composite(intersection,position)
-        elif cityMap.cityMap[y][x] == '|':
-            city.alpha_composite(up,position)
-            pass
-        elif cityMap.cityMap[y][x] == '-':
-            city.alpha_composite(left,position)
+        cityMap = gencity.City()
+        cityMap.generateCity(mapSize[0],mapSize[1])
+        
+        # Start in the middle of the map
+        startPosition = (imageSize[0] // 2, imageSize[1] // 2)
 
-city.save('city.png')
+        # Currently, these are hardcoded
+        tile_width = 132
+        tile_width_half = 64
+        tile_height = 101
+        tile_height_half = 32
+
+        for y in range(0,mapSize[1]):
+            for x in range(0,mapSize[0]):
+                position = ((x*tile_width_half)-(y*tile_width_half)+startPosition[0],(y*tile_height_half)+(x*tile_height_half)+startPosition[1])
+
+                if cityMap.cityMap[y][x] == '#':
+                    city.alpha_composite(self.intersection,position)
+                elif cityMap.cityMap[y][x] == '|':
+                    city.alpha_composite(self.up,position)
+                    pass
+                elif cityMap.cityMap[y][x] == '-':
+                    city.alpha_composite(self.left,position)
+
+        city.save('city.png')
