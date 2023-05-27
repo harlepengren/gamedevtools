6d0cc2517ea6c0c1fd42e4467d91a0057417e11e
kkoehler4070 <koehler.62@gmail.com>
2023-05-27 06:57:37 -0400

    Update cityimage.py
    Added code to correct the level types.

--- source/cityimage.py
+++ source/cityimage.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import gencity
 import xml.etree.ElementTree as ET
+import random
 
 class TileData:
     """Holds data about a tile in a spritesheet."""
@@ -8,6 +9,7 @@ class TileData:
         self.name = name
         self.location = location
         self.size = size
+        self.levelRef = None
 
     def setLocation(self,x,y):
         self.location=(x,y)
@@ -38,9 +40,11 @@ class CityImage:
         self.left = TileData('left',size=(128,64))
         self.left.image = Image.open(leftPath)
 
-    def loadBuildings(self,buildings,imgPath,xmlPath):
+    def loadBuildings(self,buildings,levels,rooves,imgPath,xmlPath):
         """buildings is the list of building numbers to load."""
         self.buildingList = []
+        self.levelDict = {}
+        self.roofList = []
         spriteSheet = Image.open(imgPath)
 
         tree = ET.parse(xmlPath)
@@ -48,12 +52,31 @@ class CityImage:
         for child in root: 
             currentNumber = int(child.attrib['name'][14:][:3])
             if currentNumber in buildings:
+                index = buildings.index(currentNumber)
+                tile = TileData(name = currentNumber)
+                tile.setLocation(int(child.attrib['x']),int(child.attrib['y']))
+                tile.setSize(int(child.attrib['width']),int(child.attrib['height']))
+                tile.loadImage(spriteSheet)
+                tile.levelRef = levels[index]
+
+                self.buildingList.append(tile)
+
+            if currentNumber in levels:
                 tile = TileData(name = currentNumber)
                 tile.setLocation(int(child.attrib['x']),int(child.attrib['y']))
                 tile.setSize(int(child.attrib['width']),int(child.attrib['height']))
                 tile.loadImage(spriteSheet)
 
-                self.buildingList.append(tile)
+                self.levelDict[str(currentNumber)] = tile
+                print("Level Dict",str(currentNumber),tile.name)
+            
+            if currentNumber in rooves:
+                tile = TileData(name = currentNumber)
+                tile.setLocation(int(child.attrib['x']),int(child.attrib['y']))
+                tile.setSize(int(child.attrib['width']),int(child.attrib['height']))
+                tile.loadImage(spriteSheet)
+
+                self.roofList.append(tile)
 
     def createCityImage(self,imageSize,mapSize):
         """Creates a new city image of size imageSize (in pixels) and with underlying mapSize (in tiles).
@@ -72,6 +95,8 @@ class CityImage:
         for y in range(0,mapSize[1]):
             for x in range(0,mapSize[0]):
                 adjustment = 0
+                hasLevels = False
+                hasRoof = False
 
                 if currentMap[y][x] == '#':
                     currentTile = self.intersection
@@ -85,11 +110,32 @@ class CityImage:
                         currentTile = self.buildingList[currentMap[y][x]]
                         adjustment = currentTile.size[1]-101
 
+                        numLevels = random.randint(0,5) * random.randint(0,1)
+                        if numLevels > 0:
+                            hasLevels = True
+                            levelTile = self.levelDict[str(currentTile.levelRef)]
+
+                        roofType = random.randint(0,len(self.roofList)+1)
+                        if roofType > 1:
+                            hasRoof = True
+                            roofTile = self.roofList[roofType-1]
+
                 tile_width_half = 64
                 tile_height_half = 32
 
                 position = ((x*tile_width_half)-(y*tile_width_half)+startPosition[0],(y*tile_height_half)+(x*tile_height_half)+startPosition[1]-adjustment)
                 city.alpha_composite(currentTile.image,position)
+
+                currentLevelPosition = position
+                if hasLevels:
+                    xAdjustment = (currentTile.size[0] - levelTile.size[0]) //2
+                    yAdjustment = 32
+                    for index in range(0,numLevels):
+                        levelPosition = (currentLevelPosition[0]+xAdjustment,currentLevelPosition[1]-yAdjustment*(index+1))
+                        city.alpha_composite(levelTile.image,levelPosition)
+
+                if hasRoof:
+                    
 
         city.save('city.png')
 
